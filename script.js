18b4989ddabfa1e8c193d220a9dd97b9f3f22419
Maiia Vyshniakova <m.vishnyakova2002@gmail.com>
2026-04-23 16:56:00 +0200

    added a carousel track with anumation

--- script.js
+++ script.js
@@ -7,71 +7,154 @@ window.isMobile = function(){
   }
 };
 
-// Adding grip items on load
-const items = getItems();
-let isMobile = window.isMobile();
-let itemsPerRow = isMobile ? 1 : 4;
-let itemsTotal = isMobile ? 4 : 8;
-let prevTotal = itemsTotal;
-updateItems(itemsPerRow, items, itemsTotal);
+/* PORTFOLIO section */
 
-// Setting up contact form
-const contactForm = document.getElementById("contact-form");
-const sendBtn = document.getElementById("send-btn");
-let isSending = false;
+const carouselImgs = [
+    { publicId: "squirel_xnm2z6", altText: "Squirel" },
+    { publicId: "bird_wdsmof", altText: "Birb" },
+    { publicId: "small-bird_vrqzfe", altText: "Small Birb" },
+    { publicId: "horse_g3aglf", altText: "Horse" },
+    { publicId: "bird_wdsmof", altText: "Birb" },
+    { publicId: "small-bird_vrqzfe", altText: "Small Birb" },
+    { publicId: "squirel_xnm2z6", altText: "Squirel" }
+];
 
-function sendEmail(event) {
-    if (event) event.preventDefault();
-    if (!contactForm.checkValidity()) {
-        contactForm.reportValidity();
-        return;
+document.addEventListener("DOMContentLoaded", () => {
+    const carouselTrack = document.querySelector('#carousel-track');
+    const carouselViewport = document.querySelector('#carousel-viewport');
+    let currentIndex = 0;
+    
+    function buildCarousel(numOfElementsDisplayed = 3) {
+        if (!carouselTrack) return;
+    
+        carouselTrack.innerHTML = '';
+        const middleIndex = Math.floor(numOfElementsDisplayed / 2);
+        currentIndex = middleIndex;
+    
+        carouselImgs.forEach((item, index) => {            
+            const img = document.createElement("img");
+            img.src = `https://res.cloudinary.com/dtvkhhwwb/image/upload/h_400,q_auto,f_auto/${item.publicId}.jpg`;
+            img.alt = item.altText;
+            img.classList.add("photo");
+            if (index === middleIndex)
+                img.classList.add("is-focused");
+    
+            // adding img to the track
+            carouselTrack.append(img);
+        });
     }
-    if (isSending)  return;
-    isSending = true;
+    
+    buildCarousel();
+    
+    // -- ADDING ANIMATION --
+    const carouselImages = Array.from(document.querySelectorAll('#carousel-track .photo'));
 
-    sendBtn.disabled = true;
-    sendBtn.innerText = "SENDING";
+    let currentTranslate = 0;
+    let isAnimating = false;
+    let dir = 1; // 1 - forward, -1 - backwards
+    
+    function updateFocus() {
+        carouselImages.forEach(img => img.classList.remove('is-focused'));
+    
+        if (currentIndex >= carouselImages.length)
+            currentIndex = carouselImgs.length - 1;
+        if (currentIndex < 0)
+            currentIndex = 0;
 
-    emailjs.init("jJxcj0PryYSoWXzXt");
+        console.log("currentIndex: ",currentIndex);
+        carouselImages[currentIndex].classList.add('is-focused');
 
-    let _name = document.getElementById("name").value;
-    let _email = document.getElementById("email").value;
-    let _message = document.getElementById("message").value;
-
-    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    if (_email === "" || _name === "" || !emailRegex.test(_email)) {
-        resetBtn();  
-        return;
+        return currentIndex;
     }
-    if (_message === "") {
-        _message = "Please contact me! " + _email + ", " + _name;
-    }
+    
+    // add caroulel 'movement'
+    const AUTO_SLIDE_INTERVAL = 3000; // 3 sec
+    let autoSlideTimer;
+    
+    function nextSlide() {
+        if (isAnimating) return;
+        isAnimating = true;
+    
+        const firstImg = carouselImages[0];
+        if (!firstImg) return;
+    
+        // calculating shift amount
+        const gap = 20;
+        const moveAmount = firstImg.getBoundingClientRect().width + gap;
+        currentTranslate -= moveAmount;
+    
+        // check for end of track
+        const trackWidth = carouselTrack.scrollWidth;
+        const viewportWidth = carouselViewport.clientWidth;
+        const maxTranslate = -(trackWidth - viewportWidth);
+        const centerOffset = (viewportWidth / 2) - firstImg.getBoundingClientRect().width;
 
-    let param = {
-        name: _name,
-        email: _email,
-        message: _message
-    }
+        // calc next position
+        let nextIndex = currentIndex + dir;
 
-    emailjs.send("service_eyvtkxd", "template_dhfp3b6", params).then(
-        function (response) {
-            console.log("SUCCESS!", response.status, response.text);
-            alert("Message sent!");
-            resetBtn(); 
-        },
-        function (error) {
-            console.log("FAILED...", error);
-            alert("Failed to send message, please try again later");
-            resetBtn();
+        // check for the first and last element
+        if (nextIndex >= carouselImages.length - 1) {
+            nextIndex = carouselImages.length - 1;
+            dir = -1;
+        } else if (nextIndex <= 0) {
+            nextIndex = 0;
+            dir = 1;
         }
-    );
-}
+    
+        currentIndex = nextIndex;
+        currentTranslate = -(currentIndex * moveAmount) + centerOffset;
 
-function resetBtn() {
-    isSending = false;
-    sendBtn.disabled = false;
-    sendBtn.innerText = "SEND";
-}
+         // check physical limit of the track
+        if (currentTranslate < maxTranslate) {
+            currentTranslate = maxTranslate;
+        }
+        if (currentTranslate > 0) {
+            currentTranslate = 0;
+        }
+
+        carouselTrack.style.transform = `translateX(${currentTranslate}px)`;
+    
+        updateFocus();
+    
+        // reset animation block after transition
+        setTimeout(() => {
+            isAnimating = false;
+        }, 500);
+    }
+    
+    // starting the animation timer
+    function startAutoSlide () {
+        // clear current timers
+        stopAutoSlide();
+        
+        autoSlideTimer = setInterval(nextSlide, AUTO_SLIDE_INTERVAL);
+    }
+    
+    // stop the timer
+    function stopAutoSlide () {
+        if (autoSlideTimer)
+            clearInterval(autoSlideTimer);
+    }
+    
+    // -- INIT CAROUSEL --
+    startAutoSlide();
+    
+    // Pause animation on hover (PC)
+    if (carouselViewport) {
+        carouselViewport.addEventListener('mouseenter', stopAutoSlide);
+        carouselViewport.addEventListener('mouseleave', startAutoSlide);
+    }
+    
+    // Pause animation on touch (Mobile)
+    if (carouselViewport) {
+        carouselViewport.addEventListener('touchstart', stopAutoSlide);
+        carouselViewport.addEventListener('touchend', () => {
+            setTimeout(startAutoSlide, 2000);
+        })
+    }
+});
+
+/* ORDER section */
 
 function getItems() {
     return [
@@ -173,6 +256,8 @@ function getItems() {
 // EXAMPLE IMG URL https://res.cloudinary.com/dtvkhhwwb/image/upload/hills_wp2zti.jpg 
 function updateItems (_itemsPerRow, _itemsArr, itemsTotal) {
     const container = document.getElementById("prints-container");
+    if (!container) return;
+
     let currentRow = null;
     const itemsPerRow = _itemsPerRow;
 
@@ -238,6 +323,14 @@ function updateItems (_itemsPerRow, _itemsArr, itemsTotal) {
     }
 }
 
+// Adding grid items on load
+const items = getItems();
+let isMobile = window.isMobile();
+let itemsPerRow = isMobile ? 1 : 4;
+let itemsTotal = isMobile ? 4 : 8;
+let prevTotal = itemsTotal;
+updateItems(itemsPerRow, items, itemsTotal);
+
 function scrollToTopRow() {
     let index = isMobile ? 0 : 1;
     const firstRow = document.getElementsByClassName("row")[index];
@@ -273,3 +366,62 @@ function onMoreBtnPressed() {
     }
 }
 
+/* CONTACT section*/
+
+// Setting up contact form
+const contactForm = document.getElementById("contact-form");
+const sendBtn = document.getElementById("send-btn");
+let isSending = false;
+
+function sendEmail(event) {
+    if (event) event.preventDefault();
+    if (!contactForm.checkValidity()) {
+        contactForm.reportValidity();
+        return;
+    }
+    if (isSending)  return;
+    isSending = true;
+
+    sendBtn.disabled = true;
+    sendBtn.innerText = "SENDING";
+
+    emailjs.init("jJxcj0PryYSoWXzXt");
+
+    let _name = document.getElementById("name").value;
+    let _email = document.getElementById("email").value;
+    let _message = document.getElementById("message").value;
+
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (_email === "" || _name === "" || !emailRegex.test(_email)) {
+        resetBtn();  
+        return;
+    }
+    if (_message === "") {
+        _message = "Please contact me! " + _email + ", " + _name;
+    }
+
+    let param = {
+        name: _name,
+        email: _email,
+        message: _message
+    }
+
+    emailjs.send("service_eyvtkxd", "template_dhfp3b6", params).then(
+        function (response) {
+            console.log("SUCCESS!", response.status, response.text);
+            alert("Message sent!");
+            resetBtn(); 
+        },
+        function (error) {
+            console.log("FAILED...", error);
+            alert("Failed to send message, please try again later");
+            resetBtn();
+        }
+    );
+}
+
+function resetBtn() {
+    isSending = false;
+    sendBtn.disabled = false;
+    sendBtn.innerText = "SEND";
+}
